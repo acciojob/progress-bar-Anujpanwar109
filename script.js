9882ab75cf57ba851d2fccc5720b59a6c727da3c
Anujpanwar109 <166197689+Anujpanwar109@users.noreply.github.com>
2025-02-19 18:36:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let circles=document.querySelectorAll(".circle");
+let bars=document.querySelectorAll(".bar");
+let nBtn=document.querySelector("#next"); 
+let pBtn=document.querySelector("#prev");
+
+let countB=0;
+
+nBtn.addEventListener("click",()=>{
+	 
+	circles[countB+1].style.background="#9883e5"
+	bars[countB].style.background="#9883e5"
+		if(countB<bars.length-1 ){
+	
+	     countB++
+	}	 
+});
+
+pBtn.addEventListener("click",()=>{ 
+	circles[countB+1].style.background="none"
+	bars[countB].style.background="none"
+		if(countB>0 ){ 
+	
+	countB--
+	}
+})
+
